4a6687833ba214c92a85da5734a04ff165bebc87
vinay021194 <61448093+vinay021194@users.noreply.github.com>
2021-12-24 16:27:47 +0530

    Updates

---
+++ precache-manifest.53d3893e39ef5c842c58737d327998f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde8084e9d117bf588c199fedec9283a",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "ed695c23b97567b8111f",
+    "url": "/food-recipe/static/css/main.ffa4cf88.chunk.css"
+  },
+  {
+    "revision": "d310b8de8575d8f79fa3",
+    "url": "/food-recipe/static/js/2.dd023780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.dd023780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed695c23b97567b8111f",
+    "url": "/food-recipe/static/js/main.ebb10368.chunk.js"
+  },
+  {
+    "revision": "56fcbae30658a5f03c21",
+    "url": "/food-recipe/static/js/runtime-main.8c69cec1.js"
+  }
+]);
